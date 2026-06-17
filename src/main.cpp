cbceb1e777ba54e897195fe5ba15156284b7d9be
joaosagrath <joaosagrath@hotmail.com>
2026-06-17 18:22:13 -0300

    Registrar as funções nativas usadas pelo menu Papyrus

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include "PCH.h"
 #include "EventHandler.h"
 #include "Settings.h"
+#include "UIExtensionsMenu.h"
 
 SKSEPluginLoad(const SKSE::LoadInterface* skse)
 {
     SKSE::Init(skse);
 
     NPCEquipmentViewer::Settings::GetSingleton().Load();
+
+    if (auto* papyrusInterface = SKSE::GetPapyrusInterface(); papyrusInterface != nullptr) {
+        papyrusInterface->Register(
+            NPCEquipmentViewer::UIExtensionsMenu::RegisterPapyrusFunctions);
+    }
 
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message) {
         if (message != nullptr && message->type == SKSE::MessagingInterface::kDataLoaded) {
